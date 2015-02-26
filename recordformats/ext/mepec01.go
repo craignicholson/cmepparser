c1d260f54dbd83c70757042d631cb61d35bc3779
Craig Nicholson <craig.nicholson@gmail.com>
2015-02-25 22:31:26 -0600

    add equipment configuration type

--- recordformats/ext/mepec01.go
+++ recordformats/ext/mepec01.go
@@ -8,3 +8,34 @@ import (
 )
 
 // MEPEC01: Equipment Configuration Type 1 – Meter configuration information
+type MEPEC01 struct {
+	RecordType          string // MEPEC01
+	RecordVersion       string // Fixed value-Release date to production. YYYYMMDD
+	SenderID            string // Fixed value, text
+	SenderCustomerID    string // Sensus code - customer:Key for flexible fields
+														 // ACME:012000
+	ReceiverID          string // Flexible field – see Table 9 for options
+	ReceiverCustomerID  string // Flexible field – see Table 9 for options
+	TimeStamp           string // Date&time this record was created YYYYMMDDHHMM
+														 // 200801310855
+	MeterID             string // Text, ABC123, Flexible field – see Table 9 for options
+	Purpose             string // Text, "OK"
+	Commodity           string // Text, Values {E,W,G,S : Electric, Water, Gas Steam}
+	Units               string // Text, KWHREG
+  SeasonIdentifier	  string // Text, ProtocolText	<empty>	Flexible field –
+                             // see Table 9 for options.
+  CalculationConstant string // float32 Multiplier to convert data values to
+														 // engineering units.
+  DateStartTime              // Date and time that the data interval reported
+                             // in this record began.  200801310000
+  DataTimeStamp              // Date and time that ends the interval reported
+                             // in this record. 200802010000
+	Interval string 					 // Time interval between readings. 00000015
+	Count    string 					 // int32 Number of triples to follow.
+
+	Triples []Interval 				 // Interval Data, Maximum of 48 allowed per record.
+                             // [n][0] Text, OnPeak: Time-of-use label. Table 5
+                             // [n][1] Text, R0 : Data quality flag. Table 6
+                             // [n][2] float64, 345.6789 : Measured Value
+
+}
--- recordformats/ext/mla01.go
+++ recordformats/ext/mla01.go
@@ -8,3 +8,44 @@ import (
 )
 
 // MLA01: Meter Level Alarms
+
+type MLA01 struct {
+	RecordType          string // MLA01
+	RecordVersion       string // Fixed value-Release date to production. YYYYMMDD
+	SenderID            string // Fixed value, Text
+	SenderCustomerID    string // Sensus code - customer:Key for flexible fields
+                             // ACME:012000
+	ReceiverID          string // Text, Flexible field – see Table 9 for options
+	ReceiverCustomerID  string // Text, Flexible field – see Table 9 for options
+	TimeStamp           string // Date&time this record was created YYYYMMDDHHMM
+                             // 200801310855
+	MeterID             string // Text, ABC123, Flexible field – see table 9 for options
+  Purpose             string // Text, "OK"
+  Commodity           string // Text, Values {E:Electric,W:Water,G:Gas,S:Steam}
+	Units               string // Fixed for this record type. {METERDQ}
+	CalculationConstant string // Always blank. Does not apply.
+                             // for this customer record type only
+
+	Interval string 					 // Usually the width of time intervals used when
+                             // all values are not time stamped. Will not apply
+                             // in this customer record type because every
+                             // triple will contain a timestamp indicating
+                             // the time of event. Always blank.
+
+  Count    string 					 // Number of alarms (triples) reported in the row.
+
+	Triples []Interval 				 // Interval Data, Maximum of 48 allowed per record.
+                             // [n][0] Text, 200801301128. Time of the Event
+                             // [n][1] Fixed as RO
+                             // [n][2] int32, 2 : Meter Alarms: Table 7
+                             // meter alarm can be smaller to reduce memory
+}
+
+// Interval Block of Data, can up Register and Interval Data
+// TODO(cn): Parse out the Bit for the Data Quality Flag, this value
+// holds a text value and a bit value, for two values with seperate meanings
+type Interval struct {
+	TimeOfEvent     string // End time of the interval
+	DataQualityFlag string // Data Quality Flag: see Table 6.
+	MeterAlarm      int32 // The measured value
+}
